090725a8cc7b7f71cc4364251fafb46db8674dcd
Austin Rovge <rovgea@msoe.edu>
2018-12-09 20:09:45 -0600

    added a resume page and a pdf to display on the page

---
+++ src/js/components/resume.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Content from './layout/content';
+
+export default class Resume extends Component {
+    render() {
+        return (
+            <Content>
+                <h1>I'm always working hard to learn more.</h1>
+               <p>This might not be the most up to date version of my resume, so beware! <a href="/assets/resume.pdf" target="_blank">Click here to view the PDF.</a></p>
+                <object data="/assets/resume.pdf" type="application/pdf">
+                    <p>Looks like there's something wrong with the embedded PDF, sorry about that! Try clicking on the link above to view it.</p>
+                </object>
+            </Content>
+        )
+    }
+}
