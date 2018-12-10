1bf4e6216141fdeb9e766e7fbdd95ad5e265d609
Austin Rovge <rovgea@msoe.edu>
2018-12-09 20:51:01 -0600

    added a photo and styling for the photo. added alt tags to all images to make rendered html valid HTML5

--- src/js/components/photography.js
+++ src/js/components/photography.js
@@ -8,8 +8,8 @@ export default class Photography extends Component {
                 <h1>I like taking pictures.</h1>
                 <p>I've been busy working on other features, so there isn't much here. I'll be able to add more content soon, but here are my two dogs in the meantime!</p>
                 <div className="photos">
-                    <img className="photo" src="/assets/kepler.jpg" />
-                    <img className="photo" src="/assets/kammi.jpg"/>
+                    <img className="photo" src="/assets/kepler.jpg" alt="My dog Kepler!"  />
+                    <img className="photo" src="/assets/kammi.jpg" alt="My dog Kammi!" />
                 </div>
             </Content>
         )
