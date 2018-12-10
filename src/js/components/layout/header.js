f74c77c4d167ecea96a9c5a527e0a778584cf7da
Austin Rovge <rovgea@msoe.edu>
2018-12-09 19:21:11 -0600

    added header and footer files to content pages. changed home arrow syntax definition back to extending a react component as it is not official yet

---
+++ src/js/components/layout/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export default class Header extends Component {
+    render() {
+        return (
+            <div className="header">
+                <h1>Austin Rovge</h1>
+                <div className="links">
+                    <Link to="/">Home</Link>
+                    <Link to="/projects">Projects</Link>
+                </div>
+            </div>
+        )
+    }
+}
