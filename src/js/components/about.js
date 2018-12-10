95038ae9bf453cfcd0e872eb269078ffd4e79f61
Austin Rovge <rovgea@msoe.edu>
2018-12-09 20:45:59 -0600

    updated text on about page to be more descriptive and about me

--- src/js/components/about.js
+++ src/js/components/about.js
@@ -6,9 +6,12 @@ export default class About extends Component {
         return (
             <Content>
                 <h1>Hi, I'm Austin.</h1>
-                <p>I enjoy programming, especially web development.</p>
-                <p>I currently attend the <a href="https://www.msoe.edu/">Milwaukee School of Engineering</a> for Sofware Engineering.</p>
-            </Content>
+                <p>I'm a college student who's working on learning more about programming.</p>
+                <p>I first started programming in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" target="_blank">Codeacademy course on JavaScript</a>. Within an hour of starting the course, I was proudly showing my mom the basic for loops I made!</p>
+                <p>In high school, I joined robotics to help program robots in C++, but I ended up switching gears and helping out with the team's web department. I helped work on <a href="https://atthecontrol.com" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" target="_blank">FIRST Robotics Competition</a> events.</p>
+                <p>After graduating from high school, I went to the <a href="https://www.msoe.edu" target="_blank">Milwaukee School of Engineering</a> to major in software engineering.</p>
+                <p>This website has some information about me and projects I've worked on, but I mostly use it to try out new web technologies!</p>
+               </Content>
         )
     }
 }
