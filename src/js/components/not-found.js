e9f69750dfdf8da5458621e12a6e0dc8c0fe3a6b
Austin Rovge <rovgea@msoe.edu>
2018-12-09 19:05:02 -0600

    moved components into their own files

---
+++ src/js/components/not-found.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React from "react";
+
+export default class NotFound extends Component {
+    render() {
+        return (
+            <h1>404 Not Found</h1>
+        )
+    }
+}
---
+++ src/js/components/projects.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+import React from "react";
+
+export default class Projects extends Component {
+    render() {
+        return (
+            <h1>Here are my projects</h1>
+        )
+    }
+}
