c9d150194f3ac7ed6f42120ab136637b0fcd66ab
Austin Rovge <rovgea@msoe.edu>
2018-12-09 20:00:05 -0600

    added redirect for about page

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import { render } from 'react-dom';
 import About from './components/about';
 import Projects from './components/projects';
@@ -9,6 +9,7 @@ render((
     <Router>
         <Switch>
             <Route exact path="/" component={About} />
+            <Redirect exact from="/about" to="/" />
             <Route exact path="/projects" component={Projects} />
             <Route path="*" component={NotFound} />
         </Switch>
