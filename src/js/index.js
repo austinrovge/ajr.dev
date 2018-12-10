070c2d8fd379ad8b8b586f5cfc5d6c902adc1c5b
Austin Rovge <rovgea@msoe.edu>
2018-12-09 18:55:05 -0600

    added routing support

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,33 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { render } from 'react-dom';
 
-class Home extends Component {
+const Home = () => (
+    <h1>Hello world!</h1>
+)
+
+class Projects extends Component {
     render() {
         return (
-            <h1>Hello world!</h1>
+            <h1>Here are my projects</h1>
         )
     }
 }
 
-render(<Home />, document.getElementById('root'));
+class NotFound extends Component {
+    render() {
+        return (
+            <h1>404 Not Found</h1>
+        )
+    }
+}
+
+render((
+    <Router>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={Projects} />
+            <Route path="*" component={NotFound} />
+        </Switch>
+    </Router>
+), document.getElementById('root'));
