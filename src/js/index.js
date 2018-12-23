2ed70cd4c72d26f9844fc6838cb2b0a90c5f7b9f
austinrovge <austinrovge@gmail.com>
2018-12-22 18:38:05 -0600

    updated rendered tags for the header/content/and footer. updated webpack config to use ES6 imports. updated text color and small icon position updates

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import { render } from 'react-dom'
 import About from './components/about'
@@ -7,21 +7,15 @@ import Resume from './components/resume'
 import Photography from './components/photography'
 import NotFound from './components/not-found'
 
-class App extends Component {
-    render() {
-        return(
-            <Router>
-                <Switch>
-                    <Route exact path="/" component={About} />
-                    <Redirect exact from="/about" to="/" />
-                    <Route exact path="/projects" component={Projects} />
-                    <Route exact path="/photography" component={Photography} />
-                    <Route exact path="/resume" component={Resume} />
-                    <Route path="*" component={NotFound} />
-                </Switch>
-            </Router>
-        )
-    }
-}
-
-render(<App />, document.getElementById('root'))
+render((
+    <Router>
+        <Switch>
+            <Route exact path="/" component={About} />
+            <Redirect exact from="/about" to="/" />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/photography" component={Photography} />
+            <Route exact path="/resume" component={Resume} />
+            <Route path="*" component={NotFound} />
+        </Switch>
+    </Router>
+), document.getElementById('root'))
