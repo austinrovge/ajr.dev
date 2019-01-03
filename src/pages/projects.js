26fa8afbc50efe1b664ee4ae74b86fd13125a0b7
austinrovge <austinrovge@gmail.com>
2019-01-03 17:35:10 -0600

    tweaked query to only grab pinned repositories. removed static content from projects page

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -11,26 +11,20 @@ export default class Projects extends Component {
                 <h1>Here's some stuff I've worked on.</h1>
                 <p>This isn't all of my projects that I've worked on, just some that I like!</p>
                 <div className="projects">
-                    <div className="project">
-                        <a href="https://github.com/austinrovge/austinrovge.me">austinrovge.me</a>
-                        <span className="language">
-                            <span className="indicator javascript" />
-                            <span className="text">JavaScript</span>
-                        </span>
-                        <div>
-                            <p>This project is a website built using ReactJS and Node.js.</p>
-                        </div>
-                    </div>
-                    <div className="project">
-                        <a href="https://github.com/austinrovge/pathfinder">pathfinder</a>
-                        <span className="language">
-                            <span className="indicator java" />
-                            <span className="text">Java</span>
-                        </span>
-                        <div>
-                            <p>This is a JavaFX application that draws a map of rectangles out. The user is able to draw walls and set the start/end points. When run, the program will draw a path between the two points.</p>
-                        </div>
-                    </div>
+                    {
+                        this.props.data.github.user.pinnedRepositories.edges.map((project, i) => (
+                            <div key={i} className="project">
+                                <a href={project.node.url}>{project.node.name}</a>
+                                <span className="language">
+                                    <span className={`indicator ${project.node.primaryLanguage.name.toLowerCase()}`}/>
+                                    <span className="text">{project.node.primaryLanguage.name}</span>
+                                </span>
+                                <div>
+                                    <p>{project.node.description}</p>
+                                </div>
+                            </div>
+                        ))
+                    }
                     {
                         this.props.data.allMarkdownRemark.edges.map((project, i) => (
                             <div key={i} className="project">
@@ -40,21 +34,6 @@ export default class Projects extends Component {
                                     <span className="text">{project.node.frontmatter.language}</span>
                                 </span>
                                 <div dangerouslySetInnerHTML={{ __html: project.node.html }} />
-                            </div>
-                        ))
-                    }
-
-                    {
-                        this.props.data.github.user.repositories.edges.map((project, i) => (
-                            <div key={i} className="project">
-                                <a href={project.node.url}>{project.node.name}</a>
-                                <span className="language">
-                                    <span className={`indicator ${project.node.primaryLanguage.name.toLowerCase()}`} />
-                                    <span className="text">{project.node.primaryLanguage.name}</span>
-                                </span>
-                                <div>
-                                    <p>{project.node.description }</p>
-                                </div>
                             </div>
                         ))
                     }
@@ -81,7 +60,7 @@ export const projectsQuery = graphql`
         
         github {
             user(login: "austinrovge") {
-                repositories(last:3) {
+                pinnedRepositories(last:3) {
                     totalCount
                     edges {
                         node {
