0d892621016930efdab4ee9a205cd3e10ad9b5da
austinrovge <austinrovge@gmail.com>
2019-01-03 17:47:43 -0600

    added environment file to hold a github api key. added dotenv package to load it into the gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,38 +1,54 @@
+require('dotenv').config({
+  path: '.env.api'
+})
+
 module.exports = {
   siteMetadata: {
-    title: `Austin Rovge`,
-    description: `Hi! I'm a software engineering student located in Milwaukee, WI. I'm working hard at learning new skills!`,
-    author: `@austinrovge`
+    title: 'Austin Rovge',
+    description: "Hi! I'm a software engineering student located in Milwaukee, WI. I'm working hard at learning new skills!",
+    author: '@austinrovge'
   },
   plugins: [
-    `gatsby-plugin-react-helmet`,
+    'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-filesystem',
       options: {
-        name: `images`,
+        name: 'images',
         path: `${__dirname}/src/images`
       }
     },
     {
-      resolve: `gatsby-source-filesystem`,
+      resolve: 'gatsby-source-graphql',
       options: {
-        name: `projects`,
+        typeName: 'GitHub',
+        fieldName: 'github',
+        url: 'https://api.github.com/graphql',
+        headers: {
+          Authorization: `bearer ${process.env.GITHUB_API_KEY}`
+        }
+        // refetchInterval: 60
+      }
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'projects',
         path: `${__dirname}/content/projects`
       }
     },
-    `gatsby-transformer-sharp`,
-    `gatsby-transformer-remark`,
-    `gatsby-plugin-sharp`,
+    'gatsby-transformer-sharp',
+    'gatsby-transformer-remark',
+    'gatsby-plugin-sharp',
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#2377BE`,
-        theme_color: `#2377BE`,
-        display: `minimal-ui`,
-        icon: `static/favicon.png`
+        name: 'gatsby-starter-default',
+        short_name: 'starter',
+        start_url: '/',
+        background_color: '#2377BE',
+        theme_color: '#2377BE',
+        display: 'minimal-ui',
+        icon: 'static/favicon.png'
       }
     }
   ]
