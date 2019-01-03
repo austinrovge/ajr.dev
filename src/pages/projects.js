5773101eddea8293d2ce2cd9b04fed0fca4976f6
austinrovge <austinrovge@gmail.com>
2019-01-03 16:30:37 -0600

    tweaked styling for projects and updated how markdown projects are added

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -17,7 +17,9 @@ export default class Projects extends Component {
                             <span className="indicator javascript" />
                             <span className="text">JavaScript</span>
                         </span>
-                        <p>This project is a website built using ReactJS and Node.js.</p>
+                        <div>
+                            <p>This project is a website built using ReactJS and Node.js.</p>
+                        </div>
                     </div>
                     <div className="project">
                         <a href="https://github.com/austinrovge/pathfinder">pathfinder</a>
@@ -25,7 +27,9 @@ export default class Projects extends Component {
                             <span className="indicator java" />
                             <span className="text">Java</span>
                         </span>
-                        <p>This is a JavaFX application that draws a map of rectangles out. The user is able to draw walls and set the start/end points. When run, the program will draw a path between the two points.</p>
+                        <div>
+                            <p>This is a JavaFX application that draws a map of rectangles out. The user is able to draw walls and set the start/end points. When run, the program will draw a path between the two points.</p>
+                        </div>
                     </div>
                     {
                         this.props.data.allMarkdownRemark.edges.map((project, i) => (
@@ -35,7 +39,7 @@ export default class Projects extends Component {
                                     <span className={`indicator ${project.node.frontmatter.language.toLowerCase()}`} />
                                     <span className="text">{project.node.frontmatter.language}</span>
                                 </span>
-                                <p>{project.node.excerpt}</p>
+                                <div dangerouslySetInnerHTML={{ __html: project.node.html }} />
                             </div>
                         ))
                     }
@@ -50,7 +54,7 @@ export const projectsQuery = graphql`
         allMarkdownRemark {
             edges {
                 node {
-                  excerpt(pruneLength:500)
+                  html
                   frontmatter {
                     title
                     language
