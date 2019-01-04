3b301606b56bc9e96d34d3c9d481c30d556f80d6
austinrovge <austinrovge@gmail.com>
2019-01-04 00:43:12 -0600

    added JSLint to the project. standardized spacing and formatting for all files. began updating photography component to have albums and album pages

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -5,41 +5,41 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 export default class Projects extends Component {
-    validateLanguage(language) {
-        return language !== null ? language.name : ''
-    }
+	validateLanguage(language) {
+		return language !== null ? language.name : ''
+	}
 
-    render() {
-        return (
-            <Layout>
-                <SEO title="Projects" />
-                <h1>Here's some stuff I've worked on.</h1>
-                <p>This isn't all of my projects that I've worked on, just some that I like!</p>
-                <div className="projects">
-                    {
-                        this.props.data.github.user.pinnedRepositories.edges.map((project, i) => (
-                            <Project key={i}
-                                     url={project.node.url}
-                                     name={project.node.name}
-                                     language={this.validateLanguage(project.node.primaryLanguage)}
-                                     description={project.node.description}
-                            />
-                        ))
-                    }
-                    {
-                        this.props.data.allMarkdownRemark.edges.map((project, i) => (
-                            <Project key={i}
-                                     url={project.node.frontmatter.link}
-                                     name={project.node.frontmatter.title}
-                                     language={project.node.frontmatter.language}
-                                     description={project.node.excerpt}
-                            />
-                        ))
-                    }
-                </div>
-            </Layout>
-        )
-    }
+	render() {
+		return (
+			<Layout>
+				<SEO title="Projects" />
+				<h1>Here's some stuff I've worked on.</h1>
+				<p>This isn't all of my projects that I've worked on, just some that I like!</p>
+				<div className="projects">
+					{
+						this.props.data.github.user.pinnedRepositories.edges.map((project, i) => (
+							<Project key={i}
+								url={project.node.url}
+								name={project.node.name}
+								language={this.validateLanguage(project.node.primaryLanguage)}
+								description={project.node.description}
+							/>
+						))
+					}
+					{
+						this.props.data.allMarkdownRemark.edges.map((project, i) => (
+							<Project key={i}
+								url={project.node.frontmatter.link}
+								name={project.node.frontmatter.title}
+								language={project.node.frontmatter.language}
+								description={project.node.excerpt}
+							/>
+						))
+					}
+				</div>
+			</Layout>
+		)
+	}
 }
 
 export const projectsQuery = graphql`
