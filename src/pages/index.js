226ed295286250127a5cef3b8d02627128d114d1
austinrovge <austinrovge@gmail.com>
2019-01-04 19:56:40 -0600

    small text changes to 404 and about pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,7 +12,7 @@ export default class Index extends Component {
 				<h1>Hi, I'm Austin.</h1>
                 <Img className="photo-circle" alt={this.props.data.austin.name} fluid={this.props.data.austin.childImageSharp.fluid} />
                 <div>
-					<p>I'm a college student who's working on learning more about programming.</p>
+					<p>I'm a college student who's working on learning more about programming!</p>
 					<p>I first started programming in middle school when I discovered the <a href="https://www.codecademy.com/learn/introduction-to-javascript" rel="noopener noreferrer" target="_blank">Codeacademy course on JavaScript</a>. Within an hour of starting the course, I was proudly showing my mom the basic for loops I made!</p>
 					<p>In high school, I joined robotics to help program robots in C++, but I ended up switching gears and helping out with the team's web department. I helped work on <a href="https://atthecontrol.com" rel="noopener noreferrer" target="_blank">At The Control</a>, a streaming service for <a href="https://www.firstinspires.org/robotics/frc" rel="noopener noreferrer" target="_blank">FIRST Robotics Competition</a> events.</p>
 					<p>After graduating from high school, I went to the <a href="https://www.msoe.edu" rel="noopener noreferrer" target="_blank">Milwaukee School of Engineering</a> to major in software engineering.</p>
