5ff88e3ea6383e6aa5a1f0cf340553d79e73ac93
austinrovge <austinrovge@gmail.com>
2019-01-05 13:35:54 -0600

    fixed abnormally large size of text on phones. more adjustment to embedded PDF sizes. updated styling for project components

--- src/components/project.js
+++ src/components/project.js
@@ -6,8 +6,8 @@ export default class Project extends Component {
 			<div className="project">
 				<a href={this.props.url} rel="noopener noreferrer" target="_blank">{this.props.name}</a>
 				<span className="language">
-					<span className={`indicator ${this.props.language.toLowerCase()}`}/>
 					<span className="text">{this.props.language}</span>
+					<span className={`indicator ${this.props.language.toLowerCase()}`}/>
 				</span>
 				<div><p>{this.props.description}</p></div>
 			</div>
