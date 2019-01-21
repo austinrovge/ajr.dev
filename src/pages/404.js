32e92356ba9987384c4e9ea8e3a178b308ccab1a
austinrovge <austinrovge@gmail.com>
2019-01-20 21:49:43 -0600

    updated eslint config to be two spaces instead of tabs. updated github link on footer to link to my profile instead of the project page for this site. updated resume pdf. small updated to photography text

--- src/pages/404.js
+++ src/pages/404.js
@@ -3,13 +3,13 @@ import Layout from '../components/layout'
 import SEO from '../components/seo'
 
 export default class NotFoundPage extends Component {
-	render() {
-		return (
-			<Layout>
-				<SEO title="Not Found" />
-				<h1>Looks like this page doesn't exist.</h1>
-				<p>Hopefully there isn't an old link somewhere!</p>
-			</Layout>
-		)
-	}
+  render() {
+    return (
+      <Layout>
+        <SEO title="Not Found" />
+        <h1>Looks like this page doesn't exist.</h1>
+        <p>Hopefully there isn't an old link somewhere!</p>
+      </Layout>
+    )
+  }
 }
