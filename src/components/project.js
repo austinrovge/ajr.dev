e21974b0ec7cc1d7c8222bd7585cfe506620951f
Austin Rovge <austinrovge@gmail.com>
2019-02-10 00:38:40 -0600

    fixed issue with c++ or c# projects not having the correct color indicator on the projects page

--- src/components/project.js
+++ src/components/project.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react'
 
 export default class Project extends Component {
+  sanitizeLanguage(language) {
+    return language.toLowerCase().replace('#', 'sharp').replace(/\+/g, 'plus')
+  }
+
   render() {
     return (
       <div className="project">
         <a href={this.props.url} rel="noopener noreferrer" target="_blank">{this.props.name}</a>
         <span className="language">
           <span className="text">{this.props.language}</span>
-          <span className={`indicator ${this.props.language.toLowerCase()}`}/>
+          <span className={`indicator ${this.sanitizeLanguage(this.props.language)}`}/>
         </span>
         <div><p>{this.props.description}</p></div>
       </div>
